bd2996f0a86253c556e83dce826f89b82e968545
Nathan Flint <nlflint@gmail.com>
2015-11-07 20:18:13 -0800

    Initial commit

---
+++ src/BattleshipModelInterface.java
@@ -0,0 +1,77 @@
+/**
+ * The programatic interface for the Battleship Model class.
+ * This interface supports communication with both the view
+ * and controller classes in the Battleship application.
+ *
+ * @author Flopsy Rabbit
+ * @author Mopsy Rabbit
+ * @author Cottontail Rabbit
+ * @author Cottontail Rabbit
+ */
+public interface BattleshipModelInterface {
+
+    /**
+     * Makes a shot during Play Mode.
+     * @param loc The designator for the shot
+     * @return The status of the shot. See the status constants
+     * @throws IllegalStateException The game is not in Play Mode
+     */
+    Status MarkShot(Location loc);
+    Boolean PlaceShip(ShipType ship, Location loc, Direction direction);
+}
+
+//-----
+
+/**
+ * A helper class for location
+ */
+class Location {
+    /**
+     * The row
+     */
+    char row;
+    /**
+     * The column
+     */
+    int col;
+}
+
+/**
+ * Helper enumerated type for return status. The SUNK_XXX
+ * values indicate HIT. The current player's turn continues
+ * until the return status is MISS.
+ */
+enum Status {
+    // return status, a miss
+    MISS,
+    // return status, a hit, doesn't sink a ship
+    HIT,
+    // return status, a hit, sunk destroyer  
+    SUNK_DESTROYER,
+    // return status, a hit, sunk cruiser
+    SUNK_CRUISER,
+    // return status, a hit, sunk battleship
+    SUNK_BATTLESHIP,
+    // return status, a hit, sunk aircraft carrier
+    SUNK_AIRCRAFT_CARRIER,
+    // return status, location was already played or invalid
+    DO_OVER
+};
+
+enum ShipType {
+    AIRCRACT_CARRIER,
+    BATTLESHIP,
+    CRUISER,
+    DESTROYER
+}
+
+enum Direction {
+    NORTH,
+    NORTHEAST,
+    EAST,
+    SOUTHEAST,
+    SOUTH,
+    SOUTHWEST,
+    WEST,
+    NORTHWEST
+}
