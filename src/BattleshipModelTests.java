29138ae9d76b8992f5233098848f45f81ff5641d
Nathan Flint <nlflint@gmail.com>
2015-11-15 00:27:03 -0800

    Created first real unit test for placing and viewing a destroyer.

--- src/BattleshipModelTests.java
+++ src/BattleshipModelTests.java
@@ -1,10 +1,27 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BattleshipModelTests {
-    @Test
-    public void test() {
-         assertTrue(true);
+    BattleshipModelInterface model;
+
+    @Before
+    public void BeforeEachTest() {
+        //model = new BattleshipModel();
     }
 
+    @Test
+    public void placeShip_whenPlacingValidLocation_ThenGetSquareReturnsShipAtLocaitons() {
+        model.placeShip(Player.PLAYER1, ShipType.DESTROYER, location(1, 'a'), location(1,'b'));
+
+        assertEquals(model.getSquare(Board.PLAYER1_DEFENSIVE, location(1, 'a')), Square.DESTROYER);
+        assertEquals(model.getSquare(Board.PLAYER1_DEFENSIVE, location(1, 'b')), Square.DESTROYER);
+    }
+
+    private Location location(int col, char row) {
+        Location loc = new Location();
+        loc.col = col;
+        loc.row = row;
+        return loc;
+    }
 }
