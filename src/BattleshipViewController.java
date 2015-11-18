8067f234aee3fe42f2345ec3e3ca3f1026e511f1
Jeremy Duke <jeremy@tetrapod.io>
2015-11-17 18:05:29 -0800

    board rendering and formatting against fake data.  TODO: plug in to model implementation.

---
+++ src/BattleshipViewController.java
@@ -0,0 +1,87 @@
+/**
+ * Implements Battleship game controls and display
+ * @author Duri Balat
+ * @author Brad Baumel
+ * @author Jeremy Duke
+ * @author Nathan Flynt
+ */
+
+public class BattleshipViewController {
+   //Fields
+   PlayerBoard p1off = new PlayerBoard(Board.PLAYER1_OFFENSIVE);
+   PlayerBoard p1def = new PlayerBoard(Board.PLAYER1_DEFENSIVE);
+   PlayerBoard p2off = new PlayerBoard(Board.PLAYER2_OFFENSIVE);
+   PlayerBoard p2def = new PlayerBoard(Board.PLAYER2_DEFENSIVE);
+
+//   Player p1 = new Player();
+//   Player p2 = new Player("PLayer 2");
+
+
+   public String displayBoard() {
+      PlayerBoard off;
+      PlayerBoard def;
+
+      String player = "Player 1";
+
+      if (player == "Player 1") {
+         off = p1off;
+         def = p1def;
+      } else {
+         off = p2off;
+         def = p2def;
+      }
+
+      String out;
+      //String offensive = "    =====OFFENSIVE BOARD - " + p.toString() + " =====";
+      String offensive = "    =====OFFENSIVE BOARD - " + player + " =====";
+      offensive += renderBoard(off);
+
+      //String defensive = "    =====DEFENSIVE BOARD - " + p.toString() + " =====";
+      String defensive = "    =====DEFENSIVE BOARD - " + player + " =====";
+      defensive += renderBoard(def);
+
+      out = offensive + defensive;
+      return out;
+   }
+
+   //Refactored helper method for printing current board state
+   private String renderBoard(PlayerBoard b) {
+      String out = "";
+
+      //Rows
+      for (int i = 0; i <= 10; i++) {
+         out += "\n   -";
+         //FORMATTING - Horizontal lines
+         for (int j = 1; j <= 10; j++) {
+            out += "---+";
+         }
+         if (i != 10) {
+            out += "\n" + (char)(i + 65) + "  |";
+            for (int n = 0; n < 10; n++) {
+               int row = i;
+               int col = n;
+               int cell = row * 10 + col;
+               out += " " + b.getSquare(cell) + " |";
+            }
+         }
+      }
+      out += "\n     ";
+      for (int j = 1; j <= 10; j++) {
+         out += j + "   ";
+      }
+      out += "\n\n";
+      return out;
+   }
+
+   public static void main(String[] args) {
+      //GAME COMPONENTS
+      //PURE TESTING HERE
+      BattleshipViewController bvc = new BattleshipViewController();
+      System.out.println(bvc.displayBoard());
+
+
+
+      //System.out.println(board.p1off.getDescription());
+
+   }
+}
